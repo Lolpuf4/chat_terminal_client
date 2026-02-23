53ba55cfa11f82ff84da12b1f141ca31d2225ff7
Ilia Kliukin <klyukin.ilia@gmail.com>
2026-02-23 15:50:40 -0800

    update

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import threading
 import os
 import datetime
 import time
+import sys
 
 
 from protocol.protocol import*
@@ -32,8 +33,7 @@ def connect():
             username, password = get_username_and_password()
 
     else:
-        username = input("enter a username for yourself ")
-        password = input("enter a password for yourself ")
+        username, password = get_username_and_password()
     for i in range(3):
         send_text(socket_test_client, username)
         send_text(socket_test_client, password)
@@ -54,6 +54,7 @@ def connect():
             file.close()
             print("you logged on.")
             break
+    send_text(socket_test_client, "terminal")
 
 
 
@@ -66,9 +67,7 @@ def get_old_chat():
     return data
 
 
-def print_chat(data):
-    chat_data = data[DM_name]
-
+def print_chat(chat_data):
     chat = ""
     for record in chat_data["msgs"]:
         if record["message_history.senderID"] == chat_data["senderID"]:
@@ -79,9 +78,12 @@ def print_chat(data):
     print(chat)
 
 def getdata(client):
-    while True:
+    global connected
+    while connected:
         information = recv(client)[1].encode()
+        print(information)
         if information == b"1":
+            print(information)
             break
         elif information == b"":
             continue
@@ -91,15 +93,18 @@ def getdata(client):
         file.close()
         if data["users.username"] == DM_name:
             print("\n" + data["messages.text"])
+            send_text(socket_test_client, data["messages.id"])
         else:
             print(f"you got a new message from {data["users.username"]}")
 
 
 def send_data():
-    while True:
-        msg = input("")
+    global connected
+    while connected:
+        msg = input(".")
         if msg == "exit":
-            send_error(socket_test_client, "1")
+            send_error(socket_test_client, "2")
+            connected = False
             break
         else:
             send_json(socket_test_client, {"msg": msg,  "user" : DM_name})
@@ -116,24 +121,34 @@ def get_DM_user(usernames):
     else:
         return usernames[int(result) - 1]
 
+def get_all_msg_ids(chat_data):
+    all_ids = []
+    for record in chat_data["msgs"]:
+        all_ids.append(record["messages.id"])
+    return {"all_msg_ids" : all_ids}
 
-HOST = "62.60.178.229"
-#HOST = "127.0.0.1"
+HOST = "62.60.178.229" if len(sys.argv) > 1 else "127.0.0.1"
 PORT = 10009
 
 socket_test_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_test_client.connect((HOST, PORT))
 
 connect()
+full_history = get_old_chat()
 
 while True:
-    full_history = get_old_chat()
+    connected = True
     usernames = list(full_history.keys())
     DM_name = get_DM_user(usernames)
     if DM_name == "exit":
+        send_error(socket_test_client, "1")
+        time.sleep(0.25)
+        connected = False
         break
-
-    print_chat(full_history)
+    msg_ids = get_all_msg_ids(full_history[DM_name])
+    print(msg_ids)
+    send_json(socket_test_client, msg_ids)
+    print_chat(full_history[DM_name])
 
     get_data = threading.Thread(target = getdata, args = [socket_test_client])
     get_data.start()
